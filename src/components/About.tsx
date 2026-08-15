05c0c56a7bc18b1c6efa88688d6dc8a7870c2627
amri <amuricchi@gmail.com>
2026-08-15 20:28:08 +0700

    feat(About): refactor About filler to grid. add About datas

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,41 +1,7 @@
 import { ScrollTrigger, gsap } from "@/lib/gsap"
 import { useGSAP } from "@gsap/react"
 import { useRef } from "react"
-
-const AboutPhoto = 'https://media.discordapp.net/attachments/895697574231674921/1537849484841586718/IMG-20260813-WA0002.jpg?ex=6a808971&is=6a7f37f1&hm=e1c4b9e7e27beb6286e93c917d9459124ef643dba7827607bdbb591dfe0079b2&=&format=webp&width=767&height=1024'
-
-const AboutData = {
-  first: [
-    "https://assets.codepen.io/7558/flame-glow-blur-001.jpg",
-    "https://assets.codepen.io/7558/flame-glow-blur-002.jpg",
-    "https://assets.codepen.io/7558/flame-glow-blur-003.jpg",
-  ],
-  second: [
-    "https://assets.codepen.io/7558/flame-glow-blur-004.jpg",
-    "https://assets.codepen.io/7558/flame-glow-blur-005.jpg",
-    "https://assets.codepen.io/7558/flame-glow-blur-006.jpg",
-  ],
-  third: [
-    "https://assets.codepen.io/7558/flame-glow-blur-007.jpg",
-    "https://assets.codepen.io/7558/flame-glow-blur-008.jpg",
-    "https://assets.codepen.io/7558/flame-glow-blur-009.jpg",
-  ],
-}
-
-const TitleData = [
-  { 
-    text: "Our Philosophy", 
-    className: "text-sm uppercase tracking-widest text-rose" 
-  },
-  { 
-    text: "Craft unique", 
-    className: "font-serif font-normal text-4xl" 
-  },
-  { 
-    text: "Signature", 
-    className: "text-rosedeep text-5xl italic font-curvy font-black" 
-  },
-]
+import { FillerData, AboutPhoto, TitleData } from "@/data/about"
 
 function About() {
   const sectionRef = useRef<HTMLElement>(null)
@@ -132,28 +98,30 @@ function About() {
         </div>
       ))}
 
-      <div className="filler flex flex-col gap-30 items-center my-[80svh]">
-        <div className="img-stack z-10 aspect-3/4 overflow-hidden relative rounded-sm flex items-center justify-center h-50">
-          {AboutData.first.map((item, idx) => (
-            <img key={idx} src={item} className="absolute pointer-events-none object-cover object-center" />
+      <div className="grid grid-cols-21  gap-y-40 my-[80svh] w-full">
+        <div className="img-stack z-10 col-start-3 col-span-8 row-start-1 h-[45svw] overflow-hidden relative rounded-sm ">
+          {FillerData.first.map((item, idx) => (
+            <img key={idx} src={item} className="absolute w-full h-full pointer-events-none object-cover object-center" />
           ))}
         </div>
-        <div className="img-stack z-10 aspect-4/3 overflow-hidden relative rounded-sm flex items-center justify-center w-50">
-          {AboutData.second.map((item, idx) => (
-            <img key={idx} src={item} className="absolute pointer-events-none object-cover object-center" />
+
+        <div className="img-stack z-10 col-start-12 col-span-10 row-start-2 h-[35svw] overflow-hidden relative rounded-sm">
+          {FillerData.second.map((item, idx) => (
+            <img key={idx} src={item} className="absolute w-full h-full pointer-events-none object-cover object-center" />
           ))}
         </div>
-        <div className="img-stack z-10 aspect-square overflow-hidden relative rounded-sm flex items-center justify-center h-35">
-          {AboutData.third.map((item, idx) => (
-            <img key={idx} src={item} className="absolute pointer-events-none object-cover object-center" />
+
+        <div className="img-stack z-10 col-start-4 col-span-7 row-start-3 h-[30.75svw] overflow-hidden relative rounded-sm">
+          {FillerData.third.map((item, idx) => (
+            <img key={idx} src={item} className="absolute w-full h-full pointer-events-none object-cover object-center" />
           ))}
         </div>
       </div>
 
       <div ref={footerRef} className="footer relative bg-blue-200 z-50 overflow-hidden rounded">
-        <div className="absolute inset-0 z-100 bg-rose/20 w-full h-full"></div>
-        <div className="absolute inset-0 z-100 bg-rose w-full h-full mix-blend-hue"></div>
-        <img src={AboutPhoto} className="h-[80svh] object-cover " />
+        <div className="absolute inset-0 z-100 bg-linear-to-b from-rose/90 to-rose/50 w-full h-full opacity-75"></div>
+        <div className="absolute inset-0 z-100 bg-rosedeep w-full h-full mix-blend-screen opacity-50"></div>
+        <img src={AboutPhoto} className="h-[80svh] object-cover grayscale" />
       </div>
 
     </section>
---
+++ src/data/about.ts
@@ -0,0 +1,34 @@
+export const AboutPhoto = 'https://files.catbox.moe/ea94w6.jpeg'
+
+export const FillerData = {
+  first: [
+    "https://assets.codepen.io/7558/flame-glow-blur-001.jpg",
+    "https://assets.codepen.io/7558/flame-glow-blur-002.jpg",
+    "https://assets.codepen.io/7558/flame-glow-blur-003.jpg",
+  ],
+  second: [
+    "https://assets.codepen.io/7558/flame-glow-blur-004.jpg",
+    "https://assets.codepen.io/7558/flame-glow-blur-005.jpg",
+    "https://assets.codepen.io/7558/flame-glow-blur-006.jpg",
+  ],
+  third: [
+    "https://assets.codepen.io/7558/flame-glow-blur-007.jpg",
+    "https://assets.codepen.io/7558/flame-glow-blur-008.jpg",
+    "https://assets.codepen.io/7558/flame-glow-blur-009.jpg",
+  ],
+}
+
+export const TitleData = [
+  { 
+    text: "Our Philosophy", 
+    className: "text-sm uppercase tracking-widest text-rose" 
+  },
+  { 
+    text: "Craft unique", 
+    className: "font-serif font-normal text-4xl" 
+  },
+  { 
+    text: "Signature", 
+    className: "text-rosedeep text-5xl italic font-curvy font-black" 
+  },
+]
